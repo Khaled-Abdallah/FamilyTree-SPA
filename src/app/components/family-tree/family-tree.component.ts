256a507aace3d5939f67578152e030b0ec3290d2
Abdelhakam-Abdullah <abdelhakam@appenza-studio.com>
2020-09-18 13:46:25 -0700

    ئءئءئء

--- src/app/components/family-tree/family-tree.component.ts
+++ src/app/components/family-tree/family-tree.component.ts
@@ -20,6 +20,9 @@ export class FamilyTreeComponent implements OnInit {
   data: TreeNode[];
   _parentId: Number = 0;
   userData: any;
+  userId: Number = 0;
+  showUserProfile: Boolean = false;
+  showTree: boolean= true;
 
   depthLimit = 4;
   treeId="ftree";
@@ -29,6 +32,7 @@ export class FamilyTreeComponent implements OnInit {
   res=null;
   searchText = "";
   treeData: any;
+  
 
   constructor(private route: ActivatedRoute,
     private userService: UserService,
@@ -46,137 +50,17 @@ export class FamilyTreeComponent implements OnInit {
      }
 
   ngOnInit() {
-
-  //   this.familyTree = [
-  //     {
-  //         "id": 1,
-  //         "name": "admin1",
-  //         "userImage": "b255b74e-36e0-4f54-9d2a-7a080175e217.jpg",
-  //         "children": [
-  //             {
-  //                 "id": 2,
-  //                 "name": "the",
-  //                 "userImage": null,
-  //                 "children": [
-  //                     {
-  //                         "id": 3,
-  //                         "name": "sdsds",
-  //                         "userImage": null,
-  //                         "children": [
-  //                             {
-  //                                 "id": 4,
-  //                                 "name": "qweqw",
-  //                                 "userImage": null,
-  //                                 "children": [
-  //                                     {
-  //                                         "id": 5,
-  //                                         "name": "sddscxv",
-  //                                         "userImage": null,
-  //                                         "children": [
-  //                                             {
-  //                                                 "id": 6,
-  //                                                 "name": "223ssaa",
-  //                                                 "userImage": null,
-  //                                                 "children": []
-  //                                             },
-  //                                             {
-  //                                                 "id": 7,
-  //                                                 "name": "2wsdsd",
-  //                                                 "userImage": null,
-  //                                                 "children": []
-  //                                             },
-  //                                             {
-  //                                                 "id": 8,
-  //                                                 "name": "bnnv",
-  //                                                 "userImage": null,
-  //                                                 "children": []
-  //                                             },
-  //                                             {
-  //                                                 "id": 9,
-  //                                                 "name": "iooio",
-  //                                                 "userImage": null,
-  //                                                 "children": []
-  //                                             },
-  //                                             {
-  //                                                 "id": 10,
-  //                                                 "name": "ghjgh",
-  //                                                 "userImage": null,
-  //                                                 "children": []
-  //                                             },
-  //                                             {
-  //                                                 "id": 11,
-  //                                                 "name": "ghjghjg",
-  //                                                 "userImage": null,
-  //                                                 "children": []
-  //                                             },
-  //                                             {
-  //                                                 "id": 12,
-  //                                                 "name": "test",
-  //                                                 "userImage": null,
-  //                                                 "children": []
-  //                                             }
-  //                                         ]
-  //                                     },
-  //                                     {
-  //                                         "id": 15,
-  //                                         "name": "popop",
-  //                                         "userImage": null,
-  //                                         "children": []
-  //                                     },
-  //                                     {
-  //                                         "id": 19,
-  //                                         "name": "erere",
-  //                                         "userImage": null,
-  //                                         "children": [
-  //                                             {
-  //                                                 "id": 21,
-  //                                                 "name": "adad asd",
-  //                                                 "userImage": null,
-  //                                                 "children": [
-  //                                                     {
-  //                                                         "id": 22,
-  //                                                         "name": "asf",
-  //                                                         "userImage": null,
-  //                                                         "children": []
-  //                                                     }
-  //                                                 ]
-  //                                             }
-  //                                         ]
-  //                                     }
-  //                                 ]
-  //                             },
-  //                             {
-  //                                 "id": 17,
-  //                                 "name": "2234",
-  //                                 "userImage": null,
-  //                                 "children": []
-  //                             },
-  //                             {
-  //                                 "id": 18,
-  //                                 "name": "23232",
-  //                                 "userImage": null,
-  //                                 "children": []
-  //                             }
-  //                         ]
-  //                     }
-  //                 ]
-  //             }
-  //         ]
-  //     }
-  // ]
-
     this.route.data.subscribe(data => {
       this.familyTree = data['treeList'];
     });
   }
-
-  printNode(node){
-    alert('done');
-    console.log(node);
-  }
-
   
   showUserDetails(node: any){  
+    this.userId = node.data.id;
+    this.showUserProfile = true;
+    this.showTree = false;
+    return;
+    
     this.spinner.show();
     this._parentId = node.data.id;
     this.userService.getUserInfo(node.data.id)
