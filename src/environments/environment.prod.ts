6a30eb02eded447f99e0fa091acc553bbbe408ba
Abdelhakam-Abdullah <abdelhakam@appenza-studio.com>
2020-10-11 18:21:19 +0200

    Update mobile mode

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,      
  
-  // url:      'https://api.appalthunayan.com/',
-  // apiUrl:   'https://api.appalthunayan.com/api/',
-  // imageUrl: 'https://api.appalthunayan.com/Images/'
+  url:      'https://api.appalthunayan.com/',
+  apiUrl:   'https://api.appalthunayan.com/api/',
+  imageUrl: 'https://api.appalthunayan.com/Images/'
 };
